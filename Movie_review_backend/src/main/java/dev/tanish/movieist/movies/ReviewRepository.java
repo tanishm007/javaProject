818e46bd3c6fce188e905cd9faaffcad97d6d9af
unknown <tanishm.it.20@nitj.ac.in>
2024-01-27 16:49:23 +0530

    first commit

---
+++ Movie_review_backend/src/main/java/dev/tanish/movieist/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.tanish.movieist.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
